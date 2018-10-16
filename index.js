c04203656f9dd0d013a360cb736dc3bc5a4dd253
Harlan Green-Taub <harlan.gt@gmail.com>
2018-10-16 15:37:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 function happyHolidays(string){
   return('Happy holidays!')
 }
+function say(happyHolidaysTo, firstname) {
+  console.log(`happyHolidaysTo, ${name}!`)
+  return('Happy holidays, ${name}!')
+}
